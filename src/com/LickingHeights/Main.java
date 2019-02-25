e374d9cfed156bea693b6e24bc5a6dadf0df98c8
Sohmer Lawrence <sohmer.lawrence.2021@student.lhschools.org>
2019-02-25 07:49:36 -0500

    Created Intellij

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,12 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World");
+        System.out.println("Hello Stars");
+        System.out.println("Hello Universe");
+    }
+    public static void
+}
