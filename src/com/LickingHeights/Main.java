6bb5e8308f096676dbe118555dc479f9b3a3abb0
Sohmer Lawrence <sohmer.lawrence.2021@student.lhschools.org>
2019-02-28 08:45:25 -0500

    printed Hello World

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -3,19 +3,9 @@ package com.LickingHeights;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
     printGreeting("World");
-    printGreeting("Stars");
-    printGreeting("Universe");
-    printGreeting("Moon");
-    int a = 5;
-    int b = 6;
-    System.out.println(a+b);
-    int c = a+b;
-    int d = c+a;
-    }
-    public static void printGreeting(String noun){
+
+
+}public static void printGreeting(String noun){
         System.out.println("Hello "+noun);
-
-
 }}
