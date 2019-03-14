ca870fc7870c38ffe3662af110681283b98d19c6
Sohmer Lawrence <sohmer.lawrence.2021@student.lhschools.org>
2019-03-14 08:49:47 -0400

    String flipWords

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -9,14 +9,14 @@ public class Main {
         Scanner keyboard;
         keyboard = new Scanner(System.in);
         printHelloWorld();
-         multiplyNumbers();
-         String greeting = printGreeting("ball");
-         System.out.println(greeting);
-         divideNumbers();
-         System.out.print("Give me two numbers: ");
-         int firstNumber= keyboard.nextInt();
-         int secondNumber = keyboard.nextInt();
-         System.out.println("The max number between " + firstNumber + " and " + secondNumber + " is " + max(firstNumber,secondNumber));
+        multiplyNumbers();
+        String greeting = printGreeting("ball");
+        System.out.println(greeting);
+        divideNumbers();
+        System.out.print("Give me two numbers: ");
+        int firstNumber = keyboard.nextInt();
+        int secondNumber = keyboard.nextInt();
+        System.out.println("The max number between " + firstNumber + " and " + secondNumber + " is " + max(firstNumber, secondNumber));
 
     }
 
@@ -24,52 +24,52 @@ public class Main {
         System.out.println("Hello World");
 
     }
+
     public static String printGreeting(String noun) {
-        return "Hello "+noun;
+        return "Hello " + noun;
     }
 
     public static int multiplyNumbers() {
         int firstNumber, secondNumber, total;
         firstNumber = 5;
         secondNumber = 10;
-        total = (firstNumber*secondNumber);
+        total = (firstNumber * secondNumber);
         return total;
 
     }
-    public static int divideNumbers(){
+
+    public static int divideNumbers() {
         int thirdNumber, fourthNumber, total1;
         thirdNumber = 10;
         fourthNumber = 5;
-        total1 = (thirdNumber/fourthNumber);
-        return (total1*2);
+        total1 = (thirdNumber / fourthNumber);
+        return (total1 * 2);
     }
-    public static int max(int num1, int num2){
+
+    public static int max(int num1, int num2) {
 
         int result;
         if (num1 > num2) {
             result = num1;
 
-        }
-            else{
+        } else {
             result = num2;
 
         }
-        return  result;
+        return result;
 
     }
-    public static String flip(String noun){
-    String word1;
-    String word2;
-
-
-
-
-
-        }
-
 }
 
-    //Create a method that will switch the order of two words and return it
+
+
+
+
+
+
+
+
+
 
 
 
